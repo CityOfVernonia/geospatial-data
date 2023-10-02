092b3469386feedbf9af79ca8bc6639d610132cd
City of Vernonia GIS <gis@vernonia-or.gov>
2023-10-02 13:30:54 -0700

    updated flood hazard geojson

---
+++ _data-processing/flood-hazard.js
@@ -0,0 +1,7 @@
+import { cleanFeatures } from './_geojson-utils.js';
+
+cleanFeatures('floodplain-management/base-flood-elevations.geojson');
+cleanFeatures('floodplain-management/cross-sections.geojson');
+cleanFeatures('floodplain-management/firm-panels.geojson');
+cleanFeatures('floodplain-management/flood-zones.geojson');
+cleanFeatures('floodplain-management/stream-profiles.geojson');
