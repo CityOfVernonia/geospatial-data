3ce92cb664d368f100a0b0dbf3c64abb25e8e114
City of Vernonia GIS <gis@vernonia-or.gov>
2024-02-26 07:20:51 -0800

    update annexations

--- _data-processing/annexations.js
+++ _data-processing/annexations.js
@@ -1,14 +1,38 @@
+import util from 'node:util';
+import { exec } from 'child_process';
 import fs from 'fs-extra';
+import download from 'download';
 import { queryFeatures } from '@esri/arcgis-rest-feature-service';
-import { DateTime } from 'luxon';
+// import { DateTime } from 'luxon';
 import { spatialExtent } from './_geometries.js';
+const _exec = util.promisify(exec);
 
-const file = 'boundaries/annexations.geojson';
+const geoJsonFile = 'boundaries/annexations.geojson';
+
+const serviceUrl =
+  'https://gis.columbiacountymaps.com/server/rest/services/Land_Development/Land_Use_Planning/FeatureServer/3';
+
+const imageUrl = 'https://gis.columbiacountymaps.com/annex_images/';
+
+const annexationFiles = (geojson) => {
+  geojson.features.forEach(async (feature) => {
+    const { IMAGE } = feature.properties;
+    const tiffFile = `boundaries/annexation-files/${IMAGE}`;
+    const pdfFile = `boundaries/annexation-files/${IMAGE.replace('tiff', 'pdf').replace('tif', 'pdf')}`;
+    const exists = await fs.exists(pdfFile);
+    if (!exists) {
+      const data = await download(`${imageUrl}${IMAGE}`);
+      await fs.writeFile(tiffFile, data);
+      await _exec(`tiff2pdf -z -o ${pdfFile} ${tiffFile}`);
+      await fs.remove(tiffFile);
+    }
+  });
+};
 
 const downloadAnnexations = async () => {
   try {
     const geojson = await queryFeatures({
-      url: 'https://gis.columbiacountymaps.com/server/rest/services/Land_Development/Land_Use_Planning/FeatureServer/3',
+      url: serviceUrl,
       outFields: ['*'],
       outSR: '4326',
       returnGeometry: true,
@@ -18,7 +42,12 @@ const downloadAnnexations = async () => {
       f: 'geojson',
     });
 
-    // geojson.features.forEach((feature) => {
+    annexationFiles(geojson);
+
+    /**
+     * TO DO: fix up attributes/properties
+     */
+    // geojson.features.forEach(async (feature) => {
     //   delete feature['id'];
     //   const {
     //     properties: {
@@ -33,7 +62,6 @@ const downloadAnnexations = async () => {
     //       PDFHYPERLINKID,
     //     },
     //   } = feature;
-
     //   const _properties = {
     //     case_number: CASENUMBER,
     //     name: PROJECTNAME,
@@ -45,12 +73,11 @@ const downloadAnnexations = async () => {
     //     file_id: PDFHYPERLINKID,
     //     url: `https://map1.msc.fema.gov/data/41/L/${PDFHYPERLINKID}.pdf`,
     //   };
-
     //   feature.properties = _properties;
     // });
 
     try {
-      fs.writeFile(file, JSON.stringify(geojson));
+      fs.writeFile(geoJsonFile, JSON.stringify(geojson));
     } catch (error) {
       console.log(error);
     }
